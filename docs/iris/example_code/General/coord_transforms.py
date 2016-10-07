0270e4ceef1f02f9cf6a4d4614bbe4171fa9157f
Corinne Bosley <corinne.bosley@metoffice.gov.uk>
2016-10-07 16:01:27 +0100

    notes added

--- docs/iris/example_code/General/coord_transforms.py
+++ docs/iris/example_code/General/coord_transforms.py
@@ -20,46 +20,31 @@ def main():
 
     # Load cube containing data in rotated pole CRS (RotatedGeogCS)
     fname = iris.sample_data_path('rotated_pole.nc')
+    # TODO: Transform cube data into different CRS two different ways:
+    # This should be one actual transform and one null transform, I think.
+    # Might have to use cube projection here?:
+    # http://scitools.org.uk/iris/docs/latest/whatsnew/1.0.html?highlight=transform
     cube1 = iris.load_cube(fname)
     cube2 = iris.load_cube(fname)
 
     # Define rotated pole globe projection for axes, with CRS of WGS84
+    fig = plt.figure()
     projection = ccrs.RotatedPole(globe=ccrs.Globe(datum='WGS84',
                                                    ellipse='WGS84'))
-    fig = plt.figure()
-
-# looped method, not working:
-    # ax_list = ['ax1', 'ax2']
-    # for i, ax in enumerate(ax_list):
-    #     ax = fig.add_subplot(1, 2, i+1, projection=projection)
-    #     ax.set_extent([352, 358, 52, 58])
-    #     ax.coastlines(resolution='10m')
-    #     qplt.pcolor(cube, edgecolors='black', axes=ax)
-
 
     ax1 = fig.add_subplot(121, projection=projection)
-    ax1.set_extent([352, 358, 52, 58])
+    ax1.set_extent([351, 358, 53, 59])
     qplt.pcolor(cube1, edgecolors='black', axes=ax1)
     qplt.points(cube1)
     ax1.coastlines(resolution='10m')
 
     ax2 = fig.add_subplot(122, projection=projection)
-    ax2.set_extent([352, 358, 52, 58])
+    ax2.set_extent([351, 358, 53, 59])
     qplt.pcolor(cube2, edgecolors='black', axes=ax2)
     qplt.points(cube2)
     ax2.coastlines(resolution='10m')
 
     plt.show()
-
-
-
-    print cube
-
-
-
-
-
-
 
 
 if __name__ == '__main__':
